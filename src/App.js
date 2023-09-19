5f56c726a634d4b2f6481b7aa9505e5b1560a7e5
Anzor <anzorv@gmail.com>
2023-09-19 17:14:11 +0100

    master

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { Header } from './components';
+import { Home, Cart } from './pages';
+import { Route } from 'react-router-dom';
+import { setPizzas } from './redux/actions/pizzas'
+
+function App() {
+  const dispatch = useDispatch();
+  const { items } = useSelector(({ pizzas, filters }) => {
+    return {
+      items: pizzas.items,
+      sortBy: filters.sortBy
+    }
+  });
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+    dispatch(setPizzas(data.pizzas))
+    });
+  }, [])
+
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Route path="/" render={() => <Home items={items} />} exact />
+        <Route path="/cart" component={Cart} exact />
+      </div>
+    </div>
+  );
+}
+
+export default App; 
